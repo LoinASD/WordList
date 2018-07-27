2cee8a0bbee3be7f7001b8a2622fb4b9d20552f7
LoinASD <levkiselev@gmail.com>
2018-07-27 14:33:15 +0700

    pre-set#1

--- app/src/main/java/io/cyanlab/wordlist/MainActivity.kt
+++ app/src/main/java/io/cyanlab/wordlist/MainActivity.kt
@@ -1,12 +1,15 @@
 package io.cyanlab.wordlist
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(main_toolbar)
+
     }
 }
